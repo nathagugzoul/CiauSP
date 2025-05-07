6e48b709d1bdcf85bbe8c2063cf15d27dadce9c5
Nathanaël Azoulay <48614408+nathagugzoul@users.noreply.github.com>
2025-05-07 12:24:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { OpenAI } = require('openai'); // CORRECTION : utiliser la nouvelle classe OpenAI
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(cors());
